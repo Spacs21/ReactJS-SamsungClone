5bf1284251ffd88dba9c3e700bcebd2b443589e9
Spacs21 <behaksr123@gmail.com>
2024-10-27 06:43:32 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import Header from './components/header/Header'
 import Announce from './components/announce/Announce'
 import Hero from './components/hero/Hero'
+import Products from './components/products/Products'
 
 function App() {
   return (
@@ -10,6 +11,13 @@ function App() {
     <Announce/>
     <Header/>
     <Hero/>
+    <Products
+      title={"Mobile & Computing"}
+      items={["Galaxy S24 Ultra", "Galaxy Z Fold6", "Galaxy Z Flip6", "Galaxy Tab S10 Series", "Galaxy Watch Ultra"]}
+      model="Galaxy S24 Ultra"
+      descp="Get up to $750 trade-in or up to $275 off without. Plus, double the storage on us."
+      image="https://rewardmobile.co.uk/wp-content/uploads/2024/01/Samsung-S24-Ultra-Titanium-Grey-2.png"
+    />
     </>
   )
 }
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Products.scss"
+
+const Products = ({title, items, image, model, descp}) => {
+const  execute_category = items.map((item, inx) => <li key={inx}><a key={inx} href="#">{item}</a></li>)
+  return (
+    <main>
+        <div className="container">
+            <div className="products" >
+                <div className="products__title">
+                    <h1>{title}</h1>
+                    <ul>
+                        {execute_category}
+                    </ul>
+                </div>
+                <div className="products__image">
+                    <img src={image} alt="" />
+                </div>
+                <div className="products__text">
+                    <h1>{model}</h1>
+                    <p>{descp}</p>
+                    <button>Buy Now</button>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Products
